e47624b230b1bd703dc70cd6309d91c6e47f2ecc
Ethan Chen <intervigil@gmail.com>
2020-10-22 14:38:37 +0530

    sm6250-common: Convert libhidl to blueprint
    Change-Id: Ief105a44eeb403b2fa41d391a0d72c776da13248

--- common.mk
+++ common.mk
@@ -115,9 +115,9 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
+    android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0.vendor
 
 # Hotword Enrollement
 PRODUCT_COPY_FILES += \
